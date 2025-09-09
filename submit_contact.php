5b962b72a507b802e107a65eabec69a5c6dc6ebd
Père Castore <barthoux4@gmail.com>
2025-09-09 11:52:42 +0200

    Traitez l'envoi en PHP

--- submit_contact.php
+++ submit_contact.php
@@ -27,6 +27,25 @@
             </div>
         </div>
         
+        <?php
+        // Testons si le fichier a bien été envoyé et s'il n'y a pas d'erreur
+        if (isset($_FILES['screenshot']) && $_FILES['screenshot']['error'] == 0)
+        {
+            // Testons si le fichier n'est pas trop gros
+            if ($_FILES['screenshot']['size'] <= 1000000)
+            {
+                $fileInfo = pathinfo($_FILES['screenshot']['name']);
+                $extension = $fileInfo['extension'];
+                $allowExtension = ['jpg', 'png', 'gif', 'jpeg'];
+                if (in_array($extension, $allowExtension)){
+                    // On peut valider le fichier et le stocker définitivement
+                    move_uploaded_file($_FILES['screenshot']['tmp_name'], 'uploads/' . basename($_FILES['screenshot']['name']));
+                    echo "L'envoi a bien été effectué !";
+                }
+            }
+        }
+        ?>
+
         <!--Btn de redirection page contact-->
         <a href='contact.php'>Contactez nous</a>
     </div>
