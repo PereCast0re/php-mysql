f495c2d6d1470f7f1c60d1f8b5270abf2e41150a
Père Castore <barthoux4@gmail.com>
2025-09-16 11:41:20 +0200

    Conservez les données grâce aux cookies

--- login.php
+++ login.php
@@ -5,9 +5,16 @@ if (isset($_POST['email']) && isset($_POST['password'])) {
     foreach ($users as $user) {
         if ($user['email'] == $_POST['email'] && $user['password'] == $_POST['password']) {
             $loggedUser = ['email' => $user['email'],];
+            // Cookie qui expire dans un an
+            setcookie('LOGGED_USER',$loggedUser['email'], time() + 365*24*3600,"","",true,true);
         } else {
             $errorMessage = sprintf('Les informations envoyées ne permettent pas de vous identifier : (%s/%s)', $_POST['email'], $_POST['password']);
         }
+    }
+
+    // Si le cookie est présent
+    if (isset($_COOKIE['LOGGED_USER'])) {
+        $loggedUser = ['email' => $_COOKIE['LOGGED_USER'],];
     }
 }
 ?>
